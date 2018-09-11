5a92edcecf90771f7efae6fca05bd8c6727eaf9d
Joerez <rezenbiz@gmail.com>
2018-09-11 13:58:05 -0700

    initial routes

--- server.js
+++ server.js
@@ -4,11 +4,38 @@ var express = require('express');
 const app = express();
 
 // Example stub:
-app.get('/', function (req, res) {
+app.get('/',  (req, res) => {
   res.send('hello')
 });
 
-const port = process.env.PORT || 3000
+
+app.get('/theater', (req, res) => {
+  res.send('all theatre names')
+})
+
+//get details of specific theater
+app.get('/theater/:name', (req, res) => {
+  res.json({'stub': `[${req.originalUrl}]`});
+})
+
+
+app.post('/theater/:name/reserve/', (req, res) => {
+  res.redirect('/theater/' + req.params.name)
+})
+
+app.get('/checkout', (req, res) => {
+  res.send('everything in your cart')
+})
+
+app.post('/checkout', (req, res) => {
+  res.redirect('/')
+})
+
+app.delete('/checkout', (req, res) => {
+  res.redirect('/')
+})
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Reserving seats on port ${port}!`);
